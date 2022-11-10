66494d0e420883d9a058c3e99b8373dc7494e654
Admin <admin.push@hackerrank.workspace>
2022-11-10 18:13:27 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/hackerrank/eshopping/product/dashboard/Application.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/Application.java
@@ -2,7 +2,12 @@ package com.hackerrank.eshopping.product.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@Configuration
+@EnableJpaRepositories
+@ComponentScan
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
