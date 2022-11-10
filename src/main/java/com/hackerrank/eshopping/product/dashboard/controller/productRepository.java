6998b4675170b24400b0171c7b02d75d4600c96d
Admin <admin.push@hackerrank.workspace>
2022-11-10 18:03:03 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/java/com/hackerrank/eshopping/product/dashboard/controller/productRepository.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/controller/productRepository.java
@@ -1,8 +1,12 @@
 package com.hackerrank.eshopping.product.dashboard.controller;
+
 import com.hackerrank.eshopping.product.dashboard.model.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface productRepository extends JpaRepository<Product,Long>{
 
 }
--- src/main/java/com/hackerrank/eshopping/product/dashboard/productrepo/productRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hackerrank.eshopping.product.dashboard.productrepo;
-import com.hackerrank.eshopping.product.dashboard.model.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface productRepository extends JpaRepository<Product,Long>{
-
-}
-
