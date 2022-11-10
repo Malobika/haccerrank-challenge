83f14cd19c63b8b66e5ee5062de0762a49fb2859
Admin <admin.push@hackerrank.workspace>
2022-11-10 17:14:03 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO product (id, name,category,retail_price,discounted_price,availibility) VALUES (1, 'Boots',"Footwear",162.0,132.84,True);
-INSERT INTO product (id, name,category,retail_price,discounted_price,availibility)VALUES (2, 'Boots2',"Footwear",162.0,132.84,True);
-INSERT INTO product (id, name,category,retail_price,discounted_price,availibility) VALUES (3, 'Boots3',"Footwear",162.0,132.84,True);
-INSERT INTO product (id, name,category,retail_price,discounted_price,availibility) VALUES (4, 'Boots4',"Footwear",162.0,132.84,False);
-INSERT INTO product (id, name,category,retail_price,discounted_price,availibility)VALUES (5, 'Boots5',"Footwear",162.0,132.84,True);
-
